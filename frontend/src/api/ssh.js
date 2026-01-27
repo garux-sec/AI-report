217e2eb5764da80508ed330ef278b7394945bc62
test <test@xxx.test>
2026-01-27 10:16:49 +0700

    fix: refactor sshApi to use shared axios instance and fix connection error

--- frontend/src/api/ssh.js
+++ frontend/src/api/ssh.js
@@ -1,36 +1,10 @@
-import axios from 'axios'
-
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api'
-
-const getHeaders = () => ({
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
-})
+import api from './index'
 
 export const sshApi = {
-    getConfigs: async () => {
-        const response = await axios.get(`${API_BASE_URL}/ssh-config`, getHeaders())
-        return response.data
-    },
-    createConfig: async (data) => {
-        const response = await axios.post(`${API_BASE_URL}/ssh-config`, data, getHeaders())
-        return response.data
-    },
-    updateConfig: async (id, data) => {
-        const response = await axios.put(`${API_BASE_URL}/ssh-config/${id}`, data, getHeaders())
-        return response.data
-    },
-    deleteConfig: async (id) => {
-        const response = await axios.delete(`${API_BASE_URL}/ssh-config/${id}`, getHeaders())
-        return response.data
-    },
-    testConnection: async (data) => {
-        const response = await axios.post(`${API_BASE_URL}/ssh-config/test`, data, getHeaders())
-        return response.data
-    },
-    setDefault: async (id) => {
-        const response = await axios.post(`${API_BASE_URL}/ssh-config/${id}/default`, {}, getHeaders())
-        return response.data
-    }
+    getConfigs: () => api.get('/ssh-config'),
+    createConfig: (data) => api.post('/ssh-config', data),
+    updateConfig: (id, data) => api.put(`/ssh-config/${id}`, data),
+    deleteConfig: (id) => api.delete(`/ssh-config/${id}`),
+    testConnection: (data) => api.post('/ssh-config/test', data),
+    setDefault: (id) => api.post(`/ssh-config/${id}/default`)
 }
